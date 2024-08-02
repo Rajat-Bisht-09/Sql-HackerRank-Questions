abffefc1b4491e384be66749566652e8dfc37377
Rajat Bisht <81902672+rbisht2909@users.noreply.github.com>
2024-08-02 10:22:37 +0530

    Add files via upload

---
+++ 1_Basic Select/7_Weather Observation Station 1/7_Weather Observation Station 1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `7_Weather_Observation_Station_1` (
+    `Column_1` VARCHAR(25) CHARACTER SET utf8
+);
+INSERT INTO `7_Weather_Observation_Station_1` VALUES ('SELECT NAME'),
+	('FROM CITY'),
+	('WHERE COUNTRYCODE = ''JPN''');
