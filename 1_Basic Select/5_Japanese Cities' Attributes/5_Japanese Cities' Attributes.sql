b1f6276165814fac5e933b90105ccd5d3b52c780
Rajat Bisht <81902672+rbisht2909@users.noreply.github.com>
2024-08-02 10:28:08 +0530

    Update 5_Japanese Cities' Attributes.sql

--- 1_Basic Select/5_Japanese Cities' Attributes/5_Japanese Cities' Attributes.sql
+++ 1_Basic Select/5_Japanese Cities' Attributes/5_Japanese Cities' Attributes.sql
@@ -1,6 +1,3 @@
-CREATE TABLE IF NOT EXISTS `5_Japanese_Cities_Attributes` (
-    `Column_1` VARCHAR(25) CHARACTER SET utf8
-);
-INSERT INTO `5_Japanese_Cities_Attributes` VALUES ('SELECT * '),
-	('FROM CITY'),
-	('WHERE COUNTRYCODE = ''JPN''');
+SELECT * 
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
