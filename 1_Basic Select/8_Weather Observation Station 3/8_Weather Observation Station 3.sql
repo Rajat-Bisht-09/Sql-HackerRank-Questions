c58713e0e52af08fe007c34a5bae7a1df0ba9f9e
Rajat Bisht <81902672+rbisht2909@users.noreply.github.com>
2024-08-02 10:30:21 +0530

    Update 8_Weather Observation Station 3.sql

--- 1_Basic Select/8_Weather Observation Station 3/8_Weather Observation Station 3.sql
+++ 1_Basic Select/8_Weather Observation Station 3/8_Weather Observation Station 3.sql
@@ -1,7 +1,4 @@
-CREATE TABLE IF NOT EXISTS `8_Weather_Observation_Station_3` (
-    `Column_1` VARCHAR(21) CHARACTER SET utf8,
-    `Column_2` VARCHAR(6) CHARACTER SET utf8
-);
-INSERT INTO `8_Weather_Observation_Station_3` VALUES ('SELECT DISTINCT(CITY)',NULL),
-	('FROM STATION',NULL),
-	('WHERE MOD(ID','2) = 0');
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE MOD(ID,2) = 0
+
