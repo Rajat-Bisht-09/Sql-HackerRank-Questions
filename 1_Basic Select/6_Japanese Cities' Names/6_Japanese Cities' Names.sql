91ccd4ccff846e0c6b390da0f055e057f0799742
Rajat Bisht <81902672+rbisht2909@users.noreply.github.com>
2024-08-02 10:21:25 +0530

    Add files via upload

---
+++ 1_Basic Select/6_Japanese Cities' Names/6_Japanese Cities' Names.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `6_Japanese_Cities_Names` (
+    `Column_1` VARCHAR(25) CHARACTER SET utf8
+);
+INSERT INTO `6_Japanese_Cities_Names` VALUES ('SELECT * '),
+	('FROM CITY'),
+	('WHERE COUNTRYCODE = ''JPN''');
